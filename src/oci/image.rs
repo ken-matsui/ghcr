deda06c1cc3dc7272cdbdfc97757a12ea46a6225
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-04-03 10:53:55 -0700

    Improve oci/image.rs

--- src/oci/image.rs
+++ src/oci/image.rs
@@ -12,12 +12,12 @@ pub(crate) struct Image {
 
 fn write_hash(directory: &Path, hash: &Value, filename: Option<String>) -> Result<(String, usize)> {
     let json = serde_json::to_string_pretty(&hash)?;
-    let sha256digest = sha256::digest(json.clone());
+    let json_sha256 = sha256::digest(json.clone());
     let filename = filename.unwrap_or(sha256digest.clone());
     let path = directory.join(filename);
     fs::write(path, json.clone())?;
 
-    Ok((sha256digest, json.len()))
+    Ok((json_sha256, json.len()))
 }
 
 impl Image {
@@ -27,11 +27,12 @@ impl Image {
         Ok(Self { schema })
     }
 
-    pub(crate) fn write_image_layout(&self, root: &Path) -> Result<()> {
+    pub(crate) fn write_image_layout(&self, root: &Path) -> Result<(String, i64)> {
         let image_layout = json!({ "imageLayoutVersion": "1.0.0" });
         self.schema
             .validate_schema(IMAGE_LAYOUT_SCHEMA_URI, &image_layout)?;
-        write_hash(root, &image_layout, Some("oci-layout".to_string()))?;
-        Ok(())
+        let (config_json_sha256, config_json_size) =
+            write_hash(root, &image_layout, Some("oci-layout".to_string()))?;
+        Ok((config_json_sha256, config_json_size as i64))
     }
 }
